5d7983c5f36f811936cf3d80676561b63869b3c9
Your Name <you@example.com>
2021-06-20 22:47:30 -0500

    implement search

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -23,6 +25,31 @@ public class SearchController {
         return "search";
     }
 
+    @PostMapping(value = "results")                //from form tag   V 3.3-4 two param, plus annotation
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("all") || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+        } else {                                                    //locale.ROOT? or english?
+            jobs = JobData.findByColumnAndValue(searchType.toLowerCase(Locale.ROOT), searchTerm.toLowerCase(Locale.ROOT));
+        }
+
+
+        model.addAttribute("columns", columnChoices); //from search
+        model.addAttribute("jobs", jobs);
+
+
+
+        return "search"; //from search
+
+
+    }
+
+
+
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+
 
 }
